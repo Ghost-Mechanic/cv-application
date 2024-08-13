984f1c37ebba04a0fa0e4ae27e0b6181dd59b4df
Ghost-Mechanic <158665405+Ghost-Mechanic@users.noreply.github.com>
2024-08-12 22:27:06 -0500

    Added an education section available to be added with form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,49 @@
 import { useState } from 'react';
 import './App.css';
 import GeneralInput from './General';
+import EducationInput from './Education';
+
+// this function takes in a date as returned from a form and converts it to a
+// more readable format
+function dateString(date) {
+  const dateObject = new Date(date);
+
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ]
+
+  const dateMonth = months[Number(date.substr(5)) - 1];
+  const dateYear = dateObject.getFullYear();
+
+  return `${dateMonth} ${dateYear}`;
+}
 
 function App() {
+  // states for the general information form
   const [editingGenInfo, setEditingGenInfo] = useState(false);
   const [formError, setFormError] = useState('');
   const [name, setName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [email, setEmail] = useState('');
+
+  // states for the educational information form
+  const [editingEducationInfo, setEditingEducationInfo] = useState(false);
+  const [formErrorEducation, setFormErrorEducation] = useState('');
+  const [schoolName, setSchoolName] = useState('');
+  const [studyTitle, setStudyTitle] = useState('');
+  const [fromStudyDate, setFromStudyDate] = useState('');
+  const [toStudyDate, setToStudyDate] = useState('');
 
   // handle the general information forms
   function handleNameChange(e) {
@@ -27,7 +63,6 @@ function App() {
     e.preventDefault();
 
     if (name === '') {
-      console.log('test');
       setFormError('You must include a name');
     }
     else if (phoneNumber === '') {
@@ -42,10 +77,50 @@ function App() {
     }
   }
 
+  // handle the educational information forms
+  function handleSchoolNameChange(e) {
+    setSchoolName(e.target.value);
+  }
+
+  function handleStudyTitleChange(e) {
+    setStudyTitle(e.target.value);
+  }
+
+  function handleFromStudyDateChange(e) {
+    setFromStudyDate(e.target.value);
+  }
+
+  function handleToStudyDateChange(e) {
+    setToStudyDate(e.target.value);
+  }
+
+  // handle the educational info submission while handling errors
+  function handleEducationInfoSubmit(e) {
+    e.preventDefault();
+
+    if (schoolName === '') {
+      setFormErrorEducation('You must include a school name');
+    }
+    else if (studyTitle === '') {
+      setFormErrorEducation('You must include a study title');
+    }
+    else if (fromStudyDate === '') {
+      setFormErrorEducation('You must include a from date for your education');
+    }
+    else if (toStudyDate === '') {
+      setFormErrorEducation('You must include a to date for your education');
+    }
+    else {
+      setFormErrorEducation('');
+      setEditingEducationInfo(false);
+    }
+  }
+
   return (
     <>
       
-        {name !== '' && phoneNumber !== '' && email !== '' && !editingGenInfo ? <div className="gen-info">
+        {name !== '' && phoneNumber !== '' && email !== '' && !editingGenInfo ? 
+        <div className="gen-info">
           <h1>{name}</h1>
           <div className="gen-info-contact">
             <h2>{phoneNumber}</h2>
@@ -54,12 +129,27 @@ function App() {
           </div>
         </div> : null}
 
+        {schoolName !== '' && studyTitle !== '' && fromStudyDate !== '' && toStudyDate !== '' && !editingEducationInfo ? 
+        <div>
+          <h1 className="section-title">Education</h1>
+          <div className="hl"></div>
+          <div className="school-section">
+            <div className="school-main">
+              <h2>{schoolName}</h2>
+              <h3>{studyTitle}</h3>
+            </div>
+            <div className="school-dates">
+              <h3>{dateString(fromStudyDate)} - {dateString(toStudyDate)}</h3>
+            </div>
+          </div>
+        </div> : null}
+
         <div className="edit-buttons">
-          <button onClick={() => setEditingGenInfo(!editingGenInfo)}>Edit General Information</button>
-          <button onClick={() => setEditingGenInfo(!editingGenInfo)}>Edit Education</button>
+          <button onClick={() => {setEditingGenInfo(!editingGenInfo); setEditingEducationInfo(false)}}>Edit General Information</button>
+          <button onClick={() => {setEditingEducationInfo(!editingEducationInfo); setEditingGenInfo(false)}}>Edit Education</button>
         </div>
 
-        {editingGenInfo ? 
+        {editingGenInfo && !editingEducationInfo ? 
         <div className="gen-form">
           <GeneralInput label="Name" value={name} handleChange={handleNameChange} />
           <GeneralInput label="Phone Number" value={phoneNumber} handleChange={handleNumberChange} />
@@ -67,6 +157,14 @@ function App() {
           <GeneralInput label="Submit" handleChange={handleGenInfoSubmit} error={formError} />
         </div> : null} 
       
+        {editingEducationInfo && !editingGenInfo ?
+        <div className="edu-form">
+          <EducationInput label="School Name" value={schoolName} handleChange={handleSchoolNameChange} />
+          <EducationInput label="Study Title" value={studyTitle} handleChange={handleStudyTitleChange} />
+          <EducationInput label="From" value={fromStudyDate} handleChange={handleFromStudyDateChange} />
+          <EducationInput label="To" value={toStudyDate} handleChange={handleToStudyDateChange} />
+          <EducationInput label="Submit" handleChange={handleEducationInfoSubmit} error={formErrorEducation} />
+        </div> : null}
     </>
   )
 }
--- src/Education.jsx
+++ src/Education.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 
-function EducationInput({ label, value, handleChange }) {
+function EducationInput({ label, value, handleChange, error }) {
     return (
         <div>
-
+            <form>
+                <label>
+                    {label !== "Submit" && label}
+                    {label === "School Name" && <input type="text" name="school" onChange={handleChange} value={value} required />}
+                    {label === "Study Title" && <input type="text" name="degree" onChange={handleChange} value={value} required />}
+                    {label === "From" && <input type="month" name="from" onChange={handleChange} value={value} required />}
+                    {label === "To" && <input type="month" name="to" onChange={handleChange} value={value} required />}
+                    {label === "Submit" && <input type="submit" value="Submit" onClick={handleChange} />}
+                </label>
+            </form>
+            {error !== '' && label === "Submit" && <p className="form-error">*{error}</p>}
         </div>
     )
 }
